c95562f93b8edf0a2203c0400c153d7f25847612
Yunlong <269138314@qq.com>
2016-10-25 11:40:30 +0800

    个人信息布局编写

---
+++ workspace/FuliCenter/app/src/main/java/com/wuyunlong/fulicenter/activity/PersonalInfo.java
@@ -0,0 +1,15 @@
+package com.wuyunlong.fulicenter.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wuyunlong.fulicenter.R;
+
+public class PersonalInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_info);
+    }
+}
